19188ef9ebd83d94f8e01336d3c67785465d3c89
GerardoSamayoa <gerardo.samayoa@noduu.com>
2024-10-18 09:11:15 -0600

    add: deploy azure

--- LogiDriveBE/Program.cs
+++ LogiDriveBE/Program.cs
@@ -57,11 +57,11 @@ builder.Services.AddScoped<IPermissionBao, PermissionBaoService>();
 
 
 // Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-    });
+builder.Services.AddControllers();
+    //.AddJsonOptions(options =>
+    //{
+    //    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    //});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
