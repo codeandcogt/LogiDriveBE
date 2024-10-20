3c401654e8f73b1a687d4baa28ddcbc337e1a4af
Jean <jrodriguezp31@miumg.edu.gt>
2024-10-19 18:13:03 -0600

    API TOWN CREATED

---
+++ LogiDriveBE/DAL/Dao/ITownDao.cs
@@ -0,0 +1,14 @@
+﻿using LogiDriveBE.DAL.Models.DTO;
+using LogiDriveBE.UTILS;
+
+namespace LogiDriveBE.DAL.Dao
+{
+    public interface ITownDao
+    {
+        Task<OperationResponse<TownDto>> CreateTownAsync(TownDto townDto);
+        Task<OperationResponse<TownDto>> GetTownByIdAsync(int id);
+        Task<OperationResponse<IEnumerable<TownDto>>> GetAllTownsAsync();
+        Task<OperationResponse<TownDto>> UpdateTownAsync(TownDto townDto);
+        Task<OperationResponse<bool>> DeleteTownAsync(int id);
+    }
+}
