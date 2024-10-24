2baebd65f366c190bc36dd8ab3fb51b909be9787
GerardoSamayoa <gerardo.samayoa@noduu.com>
2024-10-23 23:37:20 -0600

    add: authorice

--- LogiDriveBE/Controllers/Private/LogTrackingController.cs
+++ LogiDriveBE/Controllers/Private/LogTrackingController.cs
@@ -2,12 +2,14 @@
 using LogiDriveBE.DAL.Models;
 using LogiDriveBE.DAL.Models.DTO;
 using LogiDriveBE.UTILS;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LogiDriveBE.Controllers.Private
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class LogTrackingController : ControllerBase
     {
         private readonly ILogTrackingBao _logTrackingBao;
